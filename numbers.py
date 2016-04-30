707693ae19a2e138e8702db694f95c62da75a70f
Sergey Shutka <SergeyShutka@Sergeys-MacBook-Pro.local>
2016-04-30 18:08:15 +1000

    Improve user experience

--- numbers.py
+++ numbers.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
 
     default_workout = 'm-*-10'
 
-    print("Hi! What type of workout are you after? Default: {0}.".format(default_workout))
-
+    print("Hi! What type of a workout are you after? Default: {0}.".format(default_workout))
     workout_str = raw_input()
 
     if not workout_str or workout_str.isspace():
@@ -17,18 +16,34 @@ if __name__ == "__main__":
     workout_template = WorkoutTemplate.parse(workout_str)
     workout = Workout.generate(workout_template)
 
-    start = time.time()
+    raw_input("Great! Just press enter when you ready.")
 
     correct = 0
+    i = 1
+    wrong_answers = []
+    start = time.time()
     for rep in workout:
-        print(rep)
-        answer = int(raw_input())
+        print("({0}) {1}".format(i, rep))
 
-        if answer == rep.answer():
-            correct += 1
+        try:
+            answer = int(raw_input())
+
+            if answer == rep.answer():
+                correct += 1
+            else:
+                wrong_answers.append(str(rep))
+
+        except ValueError:
+            wrong_answers.append(str(rep))
+
+        i += 1
 
     end = time.time()
 
     print("Number of correct answers: {0} ({1}%)".format(correct, correct*100.0/workout_template.num_of_reps()))
     print("Required time: {0}".format(round(end - start, 2)))
+
+    if any(wrong_answers):
+        print("Wrong answers:")
+        print("\n".join(wrong_answers))
 
